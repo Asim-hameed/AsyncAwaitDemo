e2f997ccf2e6477d21202cd016c4e16109d3982b
Asim-hameed <asimhameed31@gmail.com>
2023-04-06 22:25:13 +0500

    Async Await implmented in .Net App

--- AsyncAwaitDemo/Program.cs
+++ AsyncAwaitDemo/Program.cs
@@ -11,12 +11,51 @@ namespace AsyncAwaitDemo
     {
         static void Main(string[] args)
         {
+            task1();
+            task2();
+            task3();
+            task4();
+            Console.ReadLine();
+        }
+        public static async void task1()
+        {
+            await Task.Run(() => 
+            {
+                Console.WriteLine("Task 1 starting....");
+                Thread.Sleep(4000);
+                Console.WriteLine("Task 1 Completed");
+            });
             
         }
-        public static void task1()
+        public static async void task2()
         {
-            Console.WriteLine("Task 1 starting....");
-            Thread.Sleep(1000);
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Task 2 starting....");
+                Thread.Sleep(2000);
+                Console.WriteLine("Task 2 Completed");
+            });
+            
+        }
+        public static async void task3()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Task 3 starting....");
+                Thread.Sleep(5000);
+                Console.WriteLine("Task 3 Completed");
+            });
+            
+        }
+        public static async void task4()
+        {
+            await Task.Run(()=>
+            {
+                Console.WriteLine("Task 4 starting....");
+                Thread.Sleep(1000);
+                Console.WriteLine("Task 4 Completed");
+            });
+           
         }
     }
 }
