459b4d1051a036122c59e66fe6ccbc79bb44c738
Asim-hameed <asimhameed31@gmail.com>
2023-04-06 22:16:13 +0500

    Add project files.

---
+++ AsyncAwaitDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public static void task1()
+        {
+            Console.WriteLine("Task 1 starting....");
+            Thread.Sleep(1000);
+        }
+    }
+}
